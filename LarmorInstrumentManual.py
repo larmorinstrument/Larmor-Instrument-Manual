9df9b60d62500238d30b8f640c00c67ba2c2967e
rmdalgliesh <robert.dalgliesh@stfc.ac.uk>
2022-11-30 10:57:16 +0000

    Add python bits to make pyproject work

---
+++ LarmorInstrumentManual.py
@@ -0,0 +1,11 @@
+"""
+LarmorInstrumentManual - dummy library to make things work
+"""
+
+__version__ = "0.1.0"
+
+def do_nothing(kind=None):
+    """
+    A function that does nothing
+    """
+    return 0
